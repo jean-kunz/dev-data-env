e5ea7d00178d8accfbd71633cbfcec60386077e8
Jean Kunz <jean.kunz@gmail.com>
2018-09-17 18:14:06 +0200

    fix bug

--- run-hub.bash
+++ run-hub.bash
@@ -1,2 +1,1 @@
-docker run -v /data/projects/dev-data-env/jupyterhub:/srv/jupyterhub -t jupyterhub/jupyterhub
 docker run -d -v /data/projects/dev-data-env/jupyterhub:/srv/jupyterhub -p 8000:8000  --name jean-hub -t jupyterhub/jupyterhub
