5348b3a8553948a1df8d2f25df81ac9ac9778409
Jean Kunz <jean.kunz@gmail.com>
2018-09-17 18:12:49 +0200

    add base jupyterhub config

--- run-hub.bash
+++ run-hub.bash
@@ -1,1 +1,2 @@
 docker run -v /data/projects/dev-data-env/jupyterhub:/srv/jupyterhub -t jupyterhub/jupyterhub
+docker run -d -v /data/projects/dev-data-env/jupyterhub:/srv/jupyterhub -p 8000:8000  --name jean-hub -t jupyterhub/jupyterhub
